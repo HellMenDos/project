1e819adebd148fd651419d76047124b96acba0f5
Кирилл Поздняков <kirillpozdnakov@MacBook-Pro-Kirill-2.local>
2023-02-09 20:16:14 +0300

    Pass new data

--- back/back/settings.py
+++ back/back/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000','https://skstroyproekt.ru']
 
 ROOT_URLCONF = 'back.urls'
 
